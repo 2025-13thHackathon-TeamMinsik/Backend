4f7ab2b575d19a6e536948293620682bf6f1ab57
anemic-girl <rlaalsrl-030404@naver.com>
2025-08-16 01:22:12 +0900

    feat:jobs.py

--- telentStorage/settings.py
+++ telentStorage/settings.py
@@ -150,3 +150,13 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+AUTH_USER_MODEL = 'accounts.User'
+
+
+
